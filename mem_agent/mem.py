0ce29c22eb0d557c72f022700021f2be7dafd072
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-12-20 17:26:58 +0530

    added memory using mem0 and qdrqant db

---
+++ mem_agent/mem.py
@@ -0,0 +1,84 @@
+from openai import OpenAI
+from mem0 import Memory
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import json
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(
+    api_key="",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+config = {
+    "version" : "v1.1",
+    "embedder" : {
+        "provider" : "openai",
+        "config" : { "api_key" : OPENAI_API_KEY, "model" : "text-embedding-3-small" }
+    },
+    "llm": {
+        "provider" : "openai",
+        "config" : { "api_key" : OPENAI_API_KEY, "model" : "gpt-4.1" }
+    },
+    "vector_store" : {
+        "provider" : "qdrant",
+        "config" : {
+            "host" : "localhost",
+            "port" : 6333
+        }
+    }
+}
+
+mem_client = Memory.from_config(config)
+
+while True:
+    user_query = input(" >>> ")
+
+    # Search for memory
+    search_memroy = mem_client.search(query=user_query, user_id="abhishek",)
+
+    memories = [
+        f"ID : {mem.get("id")}\n Memory : {mem.get("memory")}" for mem in search_memroy.get("results")
+    ]
+
+    SYSTEM_PROMPT = f"""
+    Here is the context about the user : {json.dumps(memories)}
+"""
+
+    response = client.chat.completions.create(
+        model="gemini-2.5-flash",
+        messages=[
+            {
+                "role" : "system",
+                "content" : SYSTEM_PROMPT
+            },
+            {
+                "role" : "user",
+                "content":user_query
+            },
+
+        ]
+    )
+
+
+    ai_response = response.choices[0].message.content
+
+    print("🤖", ai_response )
+
+    mem_client.add(
+        user_id="abhishek",
+        messages=[
+            {
+                "role" : "user",
+                "content" : user_query
+            },
+            {
+                "role" : "assistant",
+                "content" : ai_response
+            }
+        ]
+    )
+
+    print("<><><> Memroy has been saved <><><>")
